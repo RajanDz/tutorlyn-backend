4093ec81a1c4bde96b4fa1e9911318d3b8a96251
RajanDz <dzaferadzovicrajan@gmail.com>
2026-08-21 23:21:50 +0200

    Initial commit

---
+++ src/main/java/com/tutorlyn/tutorlyn/TutorlynApplication.java
@@ -0,0 +1,13 @@
+package com.tutorlyn.tutorlyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorlynApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorlynApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutorlyn/tutorlyn/TutorlynApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorlyn.tutorlyn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorlynApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

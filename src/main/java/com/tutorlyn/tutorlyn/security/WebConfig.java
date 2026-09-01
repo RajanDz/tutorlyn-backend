70a497c550bcaabcbaaf161ded7842ad247ea01a
RajanDz <dzaferadzovicrajan@gmail.com>
2026-09-01 23:27:35 +0200

    implement jwtUtils class and basic config

--- src/main/java/com/tutorlyn/tutorlyn/security/WebConfig.java
+++ src/main/java/com/tutorlyn/tutorlyn/security/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -16,6 +17,7 @@ public class WebConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity){
         httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(request -> request.anyRequest().permitAll());
 
---
+++ src/main/java/com/tutorlyn/tutorlyn/security/jwt/JwtUtils.java
@@ -0,0 +1,52 @@
+package com.tutorlyn.tutorlyn.security.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+
+    @Value("${app.jwtSecret}")
+    private String jwtSecret;
+    @Value("${app.expirationMS}")
+    private long expirationMs;
+    @Value("${app.authAccessTokenCookie}")
+    private String accessTokenCookieName;
+
+    private Key key(){
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public ResponseCookie generateAuthCookie(UserDetails userDetails){
+        String jwt = generateToken(userDetails);
+        return ResponseCookie
+                .from(accessTokenCookieName,jwt)
+                .path("/")
+                .secure(false) // secure will be false while we are testing it on localhost
+                .sameSite("Lax")
+                .maxAge(Duration.ofMillis(expirationMs))
+                .httpOnly(true)
+                .build();
+    }
+
+    private String generateToken(UserDetails userDetails){
+        if (userDetails.getUsername() == null ||userDetails.getUsername().isBlank()){
+            throw new IllegalArgumentException("Username not valid!");
+        }
+        return Jwts.builder()
+                .subject(userDetails.getUsername())
+                .issuedAt(Date.from(Instant.now()))
+                .expiration(Date.from(Instant.now().plusMillis(expirationMs)))
+                .signWith(key())
+                .compact();
+    }
+}
